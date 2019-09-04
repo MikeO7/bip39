cb6f1311aec735b40cd0ef4b1899bb16af99c355
MikeO7 <mike.d.ornelas@gmail.com>
2019-09-04 12:14:31 -0600

    Added address download button

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,7 @@
     var generationProcesses = [];
 
     var saveMnemonicButton = document.getElementById('save-mnemonic');
+    var saveAddressButton = document.getElementById('save-address');
 
     var DOM = {};
     DOM.privacyScreenToggle = $(".privacy-screen-toggle");
@@ -1681,7 +1682,30 @@
     }
 
 
+    function saveAddress() {
+        var textToWrite = document.getElementsByClassName('address')[0].innerText;
+        var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+        var fileNameToSaveAs = "Address.txt";
+
+        var downloadLink = document.createElement("a");
+        downloadLink.download = fileNameToSaveAs;
+        downloadLink.innerHTML = "Download File";
+        if (window.webkitURL != null) {
+            // Chrome allows the link to be clicked without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        } else {
+            // Firefox requires the link to be added to the DOM before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+        }
+
+        downloadLink.click();
+    }
+    
 saveMnemonicButton.addEventListener('click', saveMnemonic);
+saveAddressButton.addEventListener('click', saveAddress);
 
 function destroyClickedElement(event) {
   // remove the link from the DOM
